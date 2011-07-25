285a67027a0a294e373ca92caeae41e88eca66f6
Nicolas Francois <nicolas.francois@centraliens.net>
2011-07-25 14:45:58 +0000

    * Packages2packages_tb.sh: Updated list of distributions: sarge 	etch lenny changed to squeeze wheezy (i.e. oldstable dropped) 	* Packages2packages_tb.sh: Report failure to wget.

--- Packages2packages_tb.sh
+++ Packages2packages_tb.sh
@@ -5,11 +5,9 @@ DB2FILE="./packages_tb2Packages.pl"
 
 cd ~ddtp
 
-DISTS="alpha amd64 arm armel hppa hurd-i386 i386 ia64 m68k mips mipsel powerpc s390 sparc"
-#DISTS="i386"
+DISTS="amd64 armel i386 ia64 kfreebsd-i386 kfreebsd-amd64 mips mipsel powerpc s390 sparc"
 PART="main contrib"
-DISTRIBUTION="sarge etch lenny sid"
-DISTRIBUTION="etch lenny sid"
+DISTRIBUTION="squeeze wheezy sid"
 
 for distribution  in $DISTRIBUTION
 do
@@ -21,7 +19,7 @@ do
 
 			echo `date`: ${distribution}/${part}/$arch
 			[ -s $file.bz2 ] && mv $file.bz2 Packages/Packages.bz2
-			wget -P Packages -q -m -nd http://ftp.de.debian.org/debian/dists/${distribution}/${part}/binary-$arch/Packages.bz2
+			wget -P Packages -q -m -nd http://ftp.de.debian.org/debian/dists/${distribution}/${part}/binary-$arch/Packages.bz2 || echo `date`: "Failed to download ${distribution}/${part}/$arch"
 			[ -s Packages/Packages.bz2 ] && mv Packages/Packages.bz2 $file.bz2
 			echo `date`: Packages file downloaded
 		done
@@ -44,7 +42,7 @@ do
 			[ -f $file ] || echo no $file
 		done
 
-                file="Packages/Packages_${distribution}_${part}.bz2"
+		file="Packages/Packages_${distribution}_${part}.bz2"
 		$DB2FILE > $file
 	done
 done
