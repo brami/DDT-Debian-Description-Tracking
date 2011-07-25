a2ba926d8b0cad817cff9f22c82c35c1d73c7659
Nicolas Francois <nicolas.francois@centraliens.net>
2011-07-25 16:51:18 +0000

    * Packages2db.sh: Added function load_distribution() to 	commonalize the different blocks.

--- Packages2db.sh
+++ Packages2db.sh
@@ -4,66 +4,12 @@ INPORT2DB="../Packages2db.pl"
 
 cd ~ddtp/Packages
 
-
-
-#distribution="gaia"
-#	file="Packages-lliurex_${distribution}"
-#
-#	echo `date`: lliurex_${distribution}
-#
-#	bunzip2 -k -f $file.bz2
-#	echo `date`: Packages bunzip2
-#
-#	$INPORT2DB $file lliurex_$distribution
-#	echo `date`: data in db
-#
-#	rm -f $file
-
-
-PART="main contrib"
-
-#distribution="etch"
-#for part in $PART
-#do
-#		file="Packages_${distribution}_${part}"
-#
-#		echo `date`: ${distribution}/${part}
-#
-#		bunzip2 -k -f $file.bz2
-#		echo `date`: Packages bunzip2
-#
-#		$INPORT2DB $file $distribution
-#		echo `date`: data in db
-#
-#		rm -f $file
-#done
-# Regular vacuum to cut disk usage
-#psql ddtp -c "VACUUM"
-
-
-#distribution="lenny"
-#for part in $PART
-#do
-#		file="Packages_${distribution}_${part}"
-#
-#		echo `date`: ${distribution}/${part}
-#
-#		bunzip2 -k -f $file.bz2
-#		echo `date`: Packages bunzip2
-#
-#		$INPORT2DB $file $distribution
-#		echo `date`: data in db
-#
-#		rm -f $file
-#done
-# Regular vacuum to cut disk usage
-#psql ddtp -c "VACUUM"
-
-
-
-distribution="squeeze"
-for part in $PART
-do
+load_distribution ()
+{
+	distribution=$1
+	parts=$2
+	for part in $parts
+	do
 		file="Packages_${distribution}_${part}"
 
 		echo `date`: ${distribution}/${part}
@@ -71,49 +17,26 @@ do
 		bunzip2 -k -f $file.bz2
 		echo `date`: Packages bunzip2
 
+#		[ "$distribution" = "lliurex" ] && distribution="gaia"
 		$INPORT2DB $file $distribution
 		echo `date`: data in db
 
 		rm -f $file
-done
-distribution="wheezy"
-for part in $PART
-do
-		file="Packages_${distribution}_${part}"
+	done
+}
 
-		echo `date`: ${distribution}/${part}
+#load_distribution lliurex gaia
 
-		bunzip2 -k -f $file.bz2
-		echo `date`: Packages bunzip2
-
-		$INPORT2DB $file $distribution
-		echo `date`: data in db
-
-		rm -f $file
-done
-# Regular vacuum to cut disk usage
-psql ddtp -c "VACUUM"
+PARTS="main contrib"
+load_distribution squeeze $PARTS
+load_distribution wheezy $PARTS
 
 # Clear active before loading sid (which is what counts as active)
 psql ddtp -c "TRUNCATE active_tb"
 psql ddtp -c "TRUNCATE part_description_tb"
 
+load_distribution sid $PARTS
 
-distribution="sid"
-for part in $PART
-do
-		file="Packages_${distribution}_${part}"
-
-		echo `date`: ${distribution}/${part}
-
-		bunzip2 -k -f $file.bz2
-		echo `date`: Packages bunzip2
-
-		$INPORT2DB $file $distribution 
-		echo `date`: data in db
-
-		rm -f $file
-done
 # Regular vacuum to cut disk usage
 psql ddtp -c "VACUUM"
 
