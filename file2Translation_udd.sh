0185a0dce2ef664e184a7caf0c8b34fc4ee9fa8c
Nicolas Francois <nicolas.francois@centraliens.net>
2011-07-26 22:05:36 +0000

    * file2Translation_udd.sh: Added missing script.

---
+++ file2Translation_udd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+cd ~ddtp
+
+# Fetch active langs from database
+LANGS=`psql ddtp -q -A -t -c "select distinct language from translation_tb"`
+
+#DISTS="lenny squeeze wheezy sid"
+DISTS="squeeze wheezy sid"
+
+rm -rf Translation_udd
+
+for distribution in $DISTS
+do
+	sed -e "s/ [^ ][^ ]*$//" < packagelist/$distribution | sort | uniq > Packages/packagelist-$distribution
+	for lang in $LANGS
+	do
+		mkdir -p Translation_udd/dists/$distribution/main/i18n/ 
+		./file2Translation_udd.pl $distribution $lang | uniq | gzip > Translation_udd/dists/$distribution/main/i18n/Translation-$lang.gz
+		echo `date`: create the $distribution/Translation-$lang
+	done
+	cp packagelist/timestamp packagelist/timestamp.gpg Translation_udd/
+	cd Translation_udd
+	sha256sum dists/$distribution/main/i18n/Translation-* >> SHA256SUMS
+	cd ~ddtp
+done
+
+rm -rf ./www/Translation_udd
+cp -a ./Translation_udd/ ./www/
