70938a66423d5b8392dc7fd843bbc94103ca4e4c
Nicolas Francois <nicolas.francois@centraliens.net>
2011-07-28 23:24:41 +0000

    * Packages2packages_tb.sh: After failure to download, do not 	indicate success to download. 	* Packages2packages_tb.sh: Harmonize logs with a date prefix

--- Packages2packages_tb.sh
+++ Packages2packages_tb.sh
@@ -19,9 +19,13 @@ do
 
 			echo `date`: ${distribution}/${part}/$arch
 			[ -s $file.bz2 ] && mv $file.bz2 Packages/Packages.bz2
-			wget -P Packages -q -m -nd http://ftp.de.debian.org/debian/dists/${distribution}/${part}/binary-$arch/Packages.bz2 || echo `date`: "Failed to download ${distribution}/${part}/$arch" 1>&2
+			wget -P Packages -q -m -nd \
+			    http://ftp.de.debian.org/debian/dists/${distribution}/${part}/binary-$arch/Packages.bz2 && {
+				echo `date`: Packages file downloaded
+			} || {
+				echo `date`: Failed to download ${distribution}/${part}/$arch 1>&2
+			}
 			[ -s Packages/Packages.bz2 ] && mv Packages/Packages.bz2 $file.bz2
-			echo `date`: Packages file downloaded
 		done
 	done
 done
@@ -31,23 +35,26 @@ do
 	for part in $PART
 	do
 		# Clear active before loading sid (which is what counts as active)
+		echo -n "`date`: packages_tb "
 		psql ddtp -c "TRUNCATE packages_tb"
 
 		for arch in $DISTS
 		do
 			file="Packages/Packages_${distribution}_${part}_${arch}.bz2"
 
-			[ -f $file ] && echo -n `date` : $file
+			[ -f $file ] && echo -n `date`: $file
 			[ -f $file ] && bzcat $file | $INPORT2DB
 			[ -f $file ] || echo no $file
 		done
 
 		file="Packages/Packages_${distribution}_${part}"
+		echo -n "`date`: "
 		$DB2FILE $file
 		rm -f $file.bz2
 		bzip2 $file
 	done
 done
 # Regular vacuum to cut disk usage
+echo -n "`date`: "
 psql ddtp -c "VACUUM"
 
